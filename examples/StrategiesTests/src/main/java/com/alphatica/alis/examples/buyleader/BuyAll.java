f277df62943da62a54826b81b5854d750d499d8c
Łukasz Wojtów <lukasz.wojtow@gmail.com>
2026-01-13 07:55:30 +0100

    BuyBest

---
+++ examples/StrategiesTests/src/main/java/com/alphatica/alis/examples/buyleader/BuyAll.java
@@ -0,0 +1,57 @@
+package com.alphatica.alis.examples.buyleader;
+
+import com.alphatica.alis.data.layer.Layer;
+import com.alphatica.alis.data.loader.stooq.StooqLoader;
+import com.alphatica.alis.data.market.MarketData;
+import com.alphatica.alis.data.market.MarketName;
+import com.alphatica.alis.data.time.Time;
+import com.alphatica.alis.data.time.TimeMarketDataSet;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static com.alphatica.alis.data.time.TimeMarketDataFilters.STOCKS;
+
+public class BuyAll {
+	private static final String WORK_DIR = System.getProperty("user.home") + File.separator + "Alphatica" + File.separator + "stooq_gpw";
+
+	@SuppressWarnings("java:S106") // Suppress warning about 'System.out.println'
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
+		HashMap<MarketName, Position> positions = new HashMap<>();
+		double spent = 0.0;
+		double portion = 100.0;
+
+//		MarketData stooqData = StooqLoader.loadPL(WORK_DIR);
+		MarketData stooqData = StooqLoader.loadUS(WORK_DIR);
+		List<Time> times = stooqData.getTimes().stream().filter(t -> t.isAfter(new Time(2016_01_01))).toList();
+		for(Time time: times) {
+			var data = TimeMarketDataSet.build(time, stooqData);
+			var numberOfStocks = data.listUpToDateMarkets(STOCKS).size();
+			if (numberOfStocks > 100) {
+				var perStock = portion / numberOfStocks;
+				for(var market : data.listUpToDateMarkets(STOCKS)) {
+					var p = positions.computeIfAbsent(market.getMarketName(), marketName -> new Position());
+					var newSize = perStock / market.getData(Layer.OPEN, 0);
+					if (Double.isNaN(newSize)) {
+						continue;
+					}
+					p.count += newSize;
+					p.lastPrice = market.getData(Layer.OPEN, 0);
+					spent += perStock;
+				}
+			}
+		}
+		System.out.println("Spent: " + spent);
+		var total = positions.values().stream().mapToDouble(p -> p.count * p.lastPrice).sum();
+		System.out.println("Total: " + total);
+	}
+
+
+}
+
+class Position {
+	double count;
+	double lastPrice;
+}
---
+++ examples/StrategiesTests/src/main/java/com/alphatica/alis/examples/buyleader/BuyBest.java
@@ -0,0 +1,66 @@
+package com.alphatica.alis.examples.buyleader;
+
+import com.alphatica.alis.data.layer.Layer;
+import com.alphatica.alis.data.loader.stooq.StooqLoader;
+import com.alphatica.alis.data.market.MarketData;
+import com.alphatica.alis.data.market.MarketName;
+import com.alphatica.alis.data.time.Time;
+import com.alphatica.alis.data.time.TimeMarketDataSet;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static com.alphatica.alis.data.time.TimeMarketDataFilters.STOCKS;
+
+public class BuyBest {
+	private static final String WORK_DIR = System.getProperty("user.home") + File.separator + "Alphatica" + File.separator + "stooq_gpw";
+
+	@SuppressWarnings("java:S106") // Suppress warning about 'System.out.println'
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
+		HashMap<MarketName, Position> positions = new HashMap<>();
+		double spent = 0.0;
+		double portion = 100.0;
+
+		MarketData stooqData = StooqLoader.loadPL(WORK_DIR);
+		for(int len = 2; len < 500; len++) {
+			List<Time> times = stooqData.getTimes().stream().filter(t -> t.isAfter(new Time(2016_01_01))).toList();
+			for(Time time: times) {
+				var data = TimeMarketDataSet.getCached(time, stooqData);
+				var numberOfStocks = data.listUpToDateMarkets(STOCKS).size();
+				if (numberOfStocks > 100) {
+					MarketName bestName = null;
+					var bestScore = Double.NEGATIVE_INFINITY;
+					var bestPrice = 0.0;
+
+					for(var market : data.listUpToDateMarkets(STOCKS)) {
+						var opens = market.getLayer(Layer.OPEN);
+						if (opens.size() < len) {
+							continue;
+						}
+						var pos = positions.computeIfAbsent(market.getMarketName(), marketName -> new Position());
+						pos.lastPrice = market.getData(Layer.OPEN, 0);
+						var score = opens.get(0) / opens.get(len - 1);
+						if (score > bestScore) {
+							bestScore = score;
+							bestName = market.getMarketName();
+							bestPrice = opens.get(0);
+						}
+					}
+					if (bestScore < 1.0) {
+						continue;
+					}
+					var p = positions.computeIfAbsent(bestName, marketName -> new Position());
+					p.count += portion / bestPrice;
+					spent += portion;
+				}
+			}
+			var total = positions.values().stream().mapToDouble(p -> p.count * p.lastPrice).sum();
+			System.out.println("len: " + len + " " + (total / spent));
+		}
+
+	}
+
+
+}
